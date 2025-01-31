b7fc4ce1a38886325837d9ed9c1b502bd1809b64
Zorba11 <zorbathegreek111@gmail.com>
2025-01-30 22:14:06 -0500

    clerk setup

---
+++ app/cinematico/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const Cinematico = (props: Props) => {
+  return <div>Cinematico</div>;
+};
+
+export default Cinematico;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,38 @@
-import Provider from "@/app/provider";
-import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/sonner";
-import AuthWrapper from "@/components/wrapper/auth-wrapper";
-import { Analytics } from "@vercel/analytics/react";
-import { GeistSans } from "geist/font/sans";
-import type { Metadata } from "next";
-import "./globals.css";
+import Provider from '@/app/provider';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Toaster } from '@/components/ui/sonner';
+import AuthWrapper from '@/components/wrapper/auth-wrapper';
+import { Analytics } from '@vercel/analytics/react';
+import { GeistSans } from 'geist/font/sans';
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://nextstarter.xyz/"),
+  metadataBase: new URL('https://nextstarter.xyz/'),
   title: {
     default: 'Next Starter',
-    template: `%s | Next Starter`
+    template: `%s | Next Starter`,
   },
   description:
-    "The Ultimate Nextjs 15 Starter Kit for quickly building your SaaS, giving you time to focus on what really matters",
+    'The Ultimate Nextjs 15 Starter Kit for quickly building your SaaS, giving you time to focus on what really matters',
   openGraph: {
     description:
-      "The Ultimate Nextjs 15 Starter Kit for quickly building your SaaS, giving you time to focus on what really matters",
+      'The Ultimate Nextjs 15 Starter Kit for quickly building your SaaS, giving you time to focus on what really matters',
     images: [
-      "https://dwdwn8b5ye.ufs.sh/f/MD2AM9SEY8GucGJl7b5qyE7FjNDKYduLOG2QHWh3f5RgSi0c",
+      'https://dwdwn8b5ye.ufs.sh/f/MD2AM9SEY8GucGJl7b5qyE7FjNDKYduLOG2QHWh3f5RgSi0c',
     ],
-    url: "https://nextstarter.xyz/",
+    url: 'https://nextstarter.xyz/',
   },
   twitter: {
-    card: "summary_large_image",
-    title: "Nextjs Starter Kit",
+    card: 'summary_large_image',
+    title: 'Nextjs Starter Kit',
     description:
-      "The Ultimate Nextjs 15 Starter Kit for quickly building your SaaS, giving you time to focus on what really matters",
-    siteId: "",
-    creator: "@rasmickyy",
-    creatorId: "",
+      'The Ultimate Nextjs 15 Starter Kit for quickly building your SaaS, giving you time to focus on what really matters',
+    siteId: '',
+    creator: '@rasmickyy',
+    creatorId: '',
     images: [
-      "https://dwdwn8b5ye.ufs.sh/f/MD2AM9SEY8GucGJl7b5qyE7FjNDKYduLOG2QHWh3f5RgSi0c",
+      'https://dwdwn8b5ye.ufs.sh/f/MD2AM9SEY8GucGJl7b5qyE7FjNDKYduLOG2QHWh3f5RgSi0c',
     ],
   },
 };
